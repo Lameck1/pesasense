7276e57d815751d1fc8d18725681ca65bd5608b1
Lameck1 <lameck72@gmail.com>
2021-03-24 13:28:07 +0300

    Add validations for expense model
    - Name should not necessarily be unique
    - Association for belonging to a group has been made optional

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,9 @@
 class Expense < ApplicationRecord
-  belongs_to :group
+  validates :name, length: { in: 4..20 }, presence: true
+  validates :description, length: { in: 10..50 }, presence: true
+  validates :amount, numericality: true, presence: true
+  validates :user_id, presence: true
+
+  belongs_to :group, optional: true
   belongs_to :user
 end
