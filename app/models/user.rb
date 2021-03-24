b5f371c2620c589ef98b661c32e16e2eec590a70
Lameck1 <lameck72@gmail.com>
2021-03-24 16:30:01 +0300

    Add before_save callback on user model
    - Added before_save callback to Camel Case users name and also give user
    an avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  before_save { self.name = name.camelize }
+  before_save { self.icon = Faker::Avatar.image }
+  
   validates :name, length: { in: 4..20 }, presence: true, uniqueness: true
 
   has_many :groups
