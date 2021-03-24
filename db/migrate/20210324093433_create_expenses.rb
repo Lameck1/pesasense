7d336778e698c814d9a37058ad189e063d31f0b7
Lameck1 <lameck72@gmail.com>
2021-03-24 21:45:07 +0300

    Refactor expenses table to accept null group_id

--- db/migrate/20210324093433_create_expenses.rb
+++ db/migrate/20210324093433_create_expenses.rb
@@ -4,7 +4,7 @@ class CreateExpenses < ActiveRecord::Migration[6.1]
       t.string :name
       t.float :amount
       t.text :description
-      t.references :group, null: false, foreign_key: true
+      t.references :group, null: true, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
