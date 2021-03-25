97927b7e6fae6502d00728abd528a8af1a4ccc90
Lameck1 <lameck72@gmail.com>
2021-03-25 10:46:36 +0300

    Enforce Authentication
    - #logged_in? checks if user_id value in session is not NIL and thus
    user is logged in
    
    - #confirm_logged_in will run checks is a user is logged in before
    having access to parts of the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  layout 'application'
+
+  def logged_in?
+    session[:user_id] != nil
+  end
+
+  def confirm_logged_in
+    return if logged_in?
+
+    flash[:notice] = 'Please log in!'
+    redirect_to(login_path)
+  end
 end
