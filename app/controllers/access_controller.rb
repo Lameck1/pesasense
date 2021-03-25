a1e54cb84f23d8a73cb895884cad10e05c54d49c
Lameck1 <lameck72@gmail.com>
2021-03-25 09:53:32 +0300

    Add actions for User Sessions
    - #new is for new session and #create is for signing in with the newly
    created session
    
    - #destroy is signing out

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -1,4 +1,22 @@
 class AccessController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by_username(params[:username])
+    if user
+      cookies[:username] = user.username
+      session[:user_id] = user.id
+      flash[:notice] = 'Log in successful'
+      redirect_to(root_path)
+    else
+      flash[:alert] = 'Invalid User!'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Log out successful!'
+    redirect_to(login_path)
   end
 end
