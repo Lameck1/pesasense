ed6095abcbc49b6433d4e5f82dcbaba38362ab42
Lameck1 <lameck72@gmail.com>
2021-03-25 09:44:08 +0300

    Configure Restful Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'login' => 'access#new'
+  post   'login'   => 'access#create'
+  delete 'logout' => 'access#destroy'
+  
+  resources :expenses  
+  resources :users
+  resources :groups
+
+  root to: 'expenses#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
