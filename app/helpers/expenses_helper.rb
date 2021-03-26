0a99cd7f3898a7cda888669a21bac6f702da0521
Lameck1 <lameck72@gmail.com>
2021-03-26 22:45:01 +0300

    Add helper methods

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,13 @@
 module ExpensesHelper
+  def show_expense_icon(exp)
+    if exp.group
+      "<div class='p-0 m-0'> #{image_tag(exp.group.icon, class: 'group-icon  p-0 m-0')}</div>".html_safe
+    else
+      "<div class='p-0 m-0'> #{content_tag :p, '!?', class: 'group-icon  p-0 m-0'}</div>".html_safe
+    end
+  end
+
+  def create_group_link
+    link_to 'Create Group', new_group_path if @current_user.groups.empty?
+  end
 end
--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,5 @@
 module GroupsHelper
+  def show_group_icon(group)
+    "<div class='p-0 m-0'> #{image_tag(group.icon, class: 'group-icon  p-0 m-0')}</div>".html_safe if group.icon
+  end
 end
