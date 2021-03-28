cce4245a1be18cbc8ed01bb38a204ab51fcb0ecd
Lameck1 <lameck72@gmail.com>
2021-03-28 14:06:28 +0300

    Rerun migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_27_191834) do
+ActiveRecord::Schema.define(version: 2021_03_28_083721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 2021_03_27_191834) do
   create_table "expenses", force: :cascade do |t|
     t.string "name"
     t.float "amount"
-    t.text "description"
     t.bigint "group_id"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
