9925239acd2b40e268399550be74fa4fa20df412
Lameck1 <lameck72@gmail.com>
2021-03-29 02:38:28 +0300

    Rubocop refactor

--- spec/factories/expenses.rb
+++ spec/factories/expenses.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :expense do
-    name { %w[ Bread Shirt Textbook Wheelbarrow Toothpaste Kales].sample }
+    name { %w[Bread Shirt Textbook Wheelbarrow Toothpaste Kales].sample }
     amount { [200, 350, 800, 60, 700].sample }
     user_id { create(:user).id }
 
